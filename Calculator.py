34de07ffd6e4d4e5b9419d9acff99411b664df93
zuber2005 <birjubosumatary79@gmail.com>
2026-05-18 04:21:13 +0530

    Update Calculator.py
    print("AI Engineer Day 1")
    
    a = int(input("Pehla number daal bhai: "))
    b = int(input("Dusra number daal bhai: "))
    
    print("Jod:", a + b)
    print("Ghatana:", a - b)
    print("Guna:", a * b)
    print("Bhaag:", a / b)

--- Calculator.py
+++ Calculator.py
@@ -1,8 +1,2 @@
-print("AI Engineer Day 1")
-
-a = 10
-b = 5
-print("Jod:", a + b)
-print("Ghatana:", a - b)
-print("Guna:", a * b)
-print("Bhaag:", a / b)
+   a = int(input("Pehla number daal bhai: "))
+   b = int(input("Dusra number daal bhai: "))
